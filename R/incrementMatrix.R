f3e32a51c856ca513efb7f8950966ce4d309796b
Héctor Climente <hclimente@gmail.com>
2016-07-05 16:28:42 +0200

    function now accepts a scoring vector where values are checked instead of an integer
    renamed variable names

--- R/incrementMatrix.R
+++ R/incrementMatrix.R
@@ -2,14 +2,16 @@
 #' @param M data frame, colnames=patients, rownames=genes
 #' @param events data frame. Needs columns patient and gene
 #' @param inc number by which to increment
-incrementMatrix <- function(M, events, inc){
+incrementMatrix <- function(M, events, scoring){
   for(k in 1:nrow(events)){
-    g <- as.character(events$gene[k])
-    s <- as.character(events$patient[k])
-    idx.g <- which(rownames(M)==g)
-    idx.s <- which(colnames(M)==s)
-    if(!is.na(g)){ 
-      M[idx.g,idx.s] <- M[idx.g,idx.s]+inc
+    alteration <- events$alteration[k]
+    increment <- as.numeric(scoring[alteration])
+    gene <- as.character(events$gene[k])
+    patient <- as.character(events$patient[k])
+    idx.g <- which(rownames(M)==gene)
+    idx.s <- which(colnames(M)==patient)
+    if(!is.na(gene)){ 
+      M[idx.g,idx.s] <- M[idx.g,idx.s] + increment
     }
   }
   M
